08bcbe03908ef361cb33a6abf8eeabf0facd7ef1
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-30 20:53:54 +0100

    [FIX] : fix paths

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,8 +1,8 @@
 import jwt_decode from 'jwt-decode';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const jwtDecode = jwt_decode;
-export const reactRouter = { BrowserRouter, Routes, Route, useNavigate };
+export const reactRouter = { useNavigate };
   
 export enum Role {
     Traveler = 'traveler',
