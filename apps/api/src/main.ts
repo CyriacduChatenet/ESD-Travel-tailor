a91a190752702b46b2454390d51524c9a30b06ff
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-30 21:57:57 +0100

    [FIX] : fix ccors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,8 +6,7 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.API_PORT);
 }
