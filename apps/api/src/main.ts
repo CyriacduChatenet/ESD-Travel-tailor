715298bd66bcfdfa50de18f84787f6abf26b4f6c
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-30 22:24:20 +0100

    [FIX] : fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: `${process.env.VITE_APP_URL}`,
+      methods: 'GET,POST,PUT,DELETE',
+      credentials: true,
+    }),
+  );
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.API_PORT);
 }
