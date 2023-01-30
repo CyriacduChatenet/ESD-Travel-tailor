b87f606af3e1086f648db1709f92dddacf7c6a2e
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-30 20:55:58 +0100

    [FIX] : fix paths

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import { Router } from '@/app/router';
-import { store } from '@/setup/redux/store';
+import { store } from "@/setup/redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./app/pages/home";
+import { SigninPage } from "./app/pages/auth/signin";
+import { SignupPage } from "./app/pages/auth/signup";
+import { CreateAdvertiserPage } from "./app/pages/advertiser/createAdvertiser";
+import { AdvertiserDashboardPage } from "./app/pages/advertiser/advertiserDashboard";
+import { CreateAdvertPage } from "./app/pages/advert/createAdvert";
+import { EditAdvertPage } from "./app/pages/advert/editAdvert";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-    <Router/>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+          <Route path={"/signin"} element={<SigninPage />} />
+          <Route path={"/signup"} element={<SignupPage />} />
+          <Route
+            path={"/create-advertiser"}
+            element={<CreateAdvertiserPage />}
+          />
+          <Route
+            path={"/advertiser/dashboard"}
+            element={<AdvertiserDashboardPage />}
+          />
+          <Route
+            path={"/advertiser/create-advert"}
+            element={<CreateAdvertPage />}
+          />
+          <Route
+            path={"/advertiser/edit-advert/:id"}
+            element={<EditAdvertPage />}
+          />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
