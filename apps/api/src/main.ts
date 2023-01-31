6212d3fd8ff9aed18d0bc7125a03db9f4726d35d
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-31 12:13:00 +0100

    [FIX] : fix cors with interceptor

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://travel-manager-client.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
