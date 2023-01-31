f427b120354db000200d051003094901e346e11a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-31 09:36:37 +0100

    [FIX] : try to fix cors

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,31 +1,33 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { UserModule } from './user/user.module';
-// import { ConfigModule } from './config/config.module';
-// import { AdvertiserModule } from './advertiser/advertiser.module';
-// import { AdvertModule } from './advert/advert.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AdvertiserModule } from './advertiser/advertiser.module';
+import { AdvertModule } from './advert/advert.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.POSTGRESQL_DATABASE_HOST,
-    //   port: parseInt(process.env.POSTGRESQL_DATABASE_PORT),
-    //   username: process.env.POSTGRESQL_DATABASE_USERNAME,
-    //   password: process.env.POSTGRESQL_DATABASE_PASSWORD,
-    //   database: process.env.POSTGRESQL_DATABASE_NAME,
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
-    // AuthModule,
-    // UserModule,
-    // ConfigModule,
-    // AdvertiserModule,
-    // AdvertModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRESQL_DATABASE_HOST,
+      port: parseInt(process.env.POSTGRESQL_DATABASE_PORT),
+      username: process.env.POSTGRESQL_DATABASE_USERNAME,
+      password: process.env.POSTGRESQL_DATABASE_PASSWORD,
+      database: process.env.POSTGRESQL_DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+    AdvertiserModule,
+    AdvertModule,
   ],
   controllers: [AppController],
   providers: [AppService],
