f94e555a228d694a9665430659c6c0c05fdcaa34
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-31 13:28:37 +0100

    [FIX] : fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 
@@ -6,15 +7,14 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: [
-      'https://travel-manager-client.vercel.app',
-      'http://localhost:5173',
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  });
+  const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Authorization, Content-Type',
+  };
+
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.API_PORT);
 }
