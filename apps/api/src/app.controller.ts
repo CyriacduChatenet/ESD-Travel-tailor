876f898cfc67604280558d63114064a43c492f70
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-31 13:39:01 +0100

    [FIX] : fix cors

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,10 +1,8 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { HeaderInterceptor } from './auth/interceptor/header.interceptor';
 
 @Controller()
-@UseInterceptors(HeaderInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
