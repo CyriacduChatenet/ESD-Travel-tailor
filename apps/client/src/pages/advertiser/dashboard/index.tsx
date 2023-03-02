776dcbaabe1aa149a271d422db6f85c939b79eb9
Cyriac du Chatenet <83903574+CyriacduChatenet@users.noreply.github.com>
2023-03-03 00:28:23 +0100

    [FEAT] : add dashboards (#45)
    * [FEAT]: add traveler dashboard
    
    * [FEAT] add advertiser dashboard

---
+++ apps/client/src/pages/advertiser/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import { Layout } from '@/layout';
+
+const AdvertiserDashboard: NextPage = () => {
+	return (
+		<Layout>
+			<h1>Advertiser Dashboard</h1>
+		</Layout>
+	);
+};
+
+export default AdvertiserDashboard;
---
+++ apps/client/src/pages/traveler/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import { Layout } from '@/layout';
+
+const TravelerDashboard: NextPage = () => {
+	return (
+		<Layout>
+			<h1>Traveler Dashboard</h1>
+		</Layout>
+	);
+};
+
+export default TravelerDashboard;
