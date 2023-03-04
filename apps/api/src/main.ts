4c7ff383ac3d499f0149baeaea9a7e8f6bc8c5a6
Cyriac du Chatenet <83903574+CyriacduChatenet@users.noreply.github.com>
2023-03-04 17:28:58 +0100

    [FEAT] add swagger (#52)
    * [FEAT] add swagger
    
    * [FIX] files
    
    * [FIX] : swagger
    
    * [FIX] swagger
    
    * [FIX] swagger
    
    * [FIX] swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,33 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Travel tailor API')
+    .setDescription('The travel tailor API')
+    .setVersion('1.2')
+    .addTag('travel tailor')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/doc', app, document, {
+    customSiteTitle: 'Travel tailor API documentation',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/7658037?v=4&s=400',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+
   const corsOptions: CorsOptions = {
     origin: [`${process.env.CLIENT_APP_URL}`],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
