953b05af960a780cd88abeb910fd95dcc34e7131
Cyriac du Chatenet <83903574+CyriacduChatenet@users.noreply.github.com>
2023-03-05 17:50:08 +0100

    [FEAT]: add tool lib (#56)
    * [FEAT]: add tool lib
    
    * [FEAT]: add lib
    
    * [FIX]: delete eslint and prettier config files
    
    * [FIX]: update package.json
    
    * [FIX] files

---
+++ packages/tools/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="node" />
+
+const eslintConfig = require('./.eslintrc')
+const prettierConfig = require('./.prettierrc')
+
+module.exports = {
+  eslintConfig,
+  prettierConfig,
+}
