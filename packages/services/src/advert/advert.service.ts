651f5c318d75e65d2398a609a86253b2ea4a087f
Cyriac du Chatenet <83903574+CyriacduChatenet@users.noreply.github.com>
2023-03-08 19:38:29 +0100

    [FEAT]: add service (#61)

--- packages/services/src/advert/advert.service.ts
+++ packages/services/src/advert/advert.service.ts
@@ -1,21 +1,27 @@
 import { useFetch } from "@travel-tailor/hooks";
-import { CreateAdvertDTO } from "@travel-tailor/types";
+import { CreateAdvertDTO, UpdateAdvertDTO } from "@travel-tailor/types";
 
-import { AdvertiserService } from "../advertiser/advertiser.service";
+import { TokenService } from "../tokens/token.service";
 
 const findAllAdverts = async (api_url: string) => {
     return await useFetch.get(`${api_url}/advert`);
 };
 
-const findAdvertById = async (api_url: string) => {};
+const findAdvertById = async (api_url: string, id: string) => {
+    return await useFetch.get(`${api_url}/advert/${id}`);
+};
 
 const createAdvert = async (api_url: string, credentials: CreateAdvertDTO, advertiserId?: string) => {
     return await useFetch.post(`${api_url}/advert`, {...credentials, advertiserId: advertiserId});
 };
 
-const updateAdvert = async (api_url: string) => {};
+const updateAdvert = async (api_url: string, id: string, credentials: UpdateAdvertDTO) => {
+    return await useFetch.protectedPatch(`${api_url}/advert/${id}`, credentials, String(TokenService.getAccessToken()));
+};
 
-const deleteAdvert = async (api_url: string) => {};
+const deleteAdvert = async (api_url: string, id: string) => {
+    return await useFetch.protectedRemove(`${api_url}/advert/${id}`, String(TokenService.getAccessToken()));
+};
 
 export const AdvertService = {
     findAllAdverts,
