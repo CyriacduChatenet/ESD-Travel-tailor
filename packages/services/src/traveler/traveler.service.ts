24e4a8b6e145bcd515dc2940cd26c02bcbfbfb5f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-09 09:35:09 +0100

    [FEAT] add services

--- packages/services/src/traveler/traveler.service.ts
+++ packages/services/src/traveler/traveler.service.ts
@@ -1,25 +1,39 @@
-import { useFetch } from '@travel-tailor/hooks'
-import { CreateTravelerDTO, UpdateTravelerDTO } from '@travel-tailor/types'
-import { TokenService } from '../tokens/token.service'
+import { useFetch } from '@travel-tailor/hooks';
+import { CreateTravelerDTO, UpdateTravelerDTO } from '@travel-tailor/types';
 
-const createTraveler = (api_url: string, credentials: CreateTravelerDTO) => {
-  return useFetch.post(api_url, credentials)
-}
+import { TokenService } from '../tokens/token.service';
 
-const updateTraveler = (api_url: string, credentials: UpdateTravelerDTO) => {
-  return useFetch.protectedPatch(
+const findAllTravelers = async (api_url: string) => {
+  return await useFetch.get(`${api_url}/traveler`);
+};
+
+const findTravelerById = async (api_url: string, travelerId: string) => {
+  return await useFetch.get(`${api_url}/traveler/${travelerId}`);
+};
+
+const createTraveler = async (api_url: string, credentials: CreateTravelerDTO) => {
+  return await useFetch.post(api_url, credentials)
+};
+
+const updateTraveler = async (api_url: string, credentials: UpdateTravelerDTO) => {
+  return await useFetch.protectedPatch(
     `${api_url}`,
     credentials,
     String(TokenService.getSigninToken())
   )
-}
+};
 
-const getTravelerInfo = async (api_url: string, travelerId: string) => {
-  return await useFetch.get(`${api_url}/traveler/${travelerId}`);
+const deleteTraveler = async (api_url: string, travelerId: string) => {
+  return await useFetch.protectedRemove(
+    `${api_url}/${travelerId}`,
+    String(TokenService.getSigninToken())
+  )
 };
 
 export const TravelerService = {
+  findAllTravelers,
+  findTravelerById,
   createTraveler,
   updateTraveler,
-  getTravelerInfo,
+  deleteTraveler
 }
