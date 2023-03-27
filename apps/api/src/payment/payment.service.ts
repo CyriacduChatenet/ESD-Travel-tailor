5d5a7fb88189bf7cac7983813bd3442e46b6f42b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-27 16:41:46 +0200

    [FIX files

--- apps/api/src/payment/payment.service.ts
+++ apps/api/src/payment/payment.service.ts
@@ -15,7 +15,7 @@ export class PaymentService {
       card: {
         number: '4242424242424242',
         exp_month: 12,
-        exp_year: 2022,
+        exp_year: 2023,
         cvc: '123',
       },
     });
