f48f682cb4053d1377fa31b82a3d9946c8d53e1a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-30 16:43:36 +0200

    [FIX] controller

--- apps/api/src/payment/payment.controller.ts
+++ apps/api/src/payment/payment.controller.ts
@@ -7,6 +7,12 @@ import { CreatePaymentDto } from './dto/create-payment.dto'
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @Post('create-customer')
+  async createStripeCustomer() {
+    const customer = await this.paymentService.createStripeCustomer()
+    return customer
+  }
+
   @Post()
   async createPaymentIntent(@Body() createPaymentDto: CreatePaymentDto) {
     const paymentIntent = await this.paymentService.createPaymentIntent(
@@ -16,9 +22,10 @@ export class PaymentController {
   }
 
   @Post('/confirm')
-  async confirmPaymentIntent(@Body('paymentIntentId') paymentIntentId: string) {
+  async confirmPaymentIntent(@Body('paymentIntentId') paymentIntentId: string, @Body('userId') customer: string) {
     const paymentIntent = await this.paymentService.confirmPaymentIntent(
-      paymentIntentId
+      paymentIntentId,
+      customer
     )
     return paymentIntent
   }
