f338ec5cdea82a5fee26077a5f901dae151110a6
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-30 16:43:49 +0200

    [FX] modules

--- apps/api/src/payment/customer/customer.module.ts
+++ apps/api/src/payment/customer/customer.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Customer } from './entities/customer.entity';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { PaymentModule } from '../payment.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
+  imports: [TypeOrmModule.forFeature([Customer]), forwardRef(() => PaymentModule)],
   controllers: [CustomerController],
-  providers: [CustomerService]
+  providers: [CustomerService],
+  exports: [CustomerService],
 })
 export class CustomerModule {}
