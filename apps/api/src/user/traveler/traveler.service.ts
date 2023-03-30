034c6ab08cf460b28718e3f153da4f214f30318b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-03-30 16:44:48 +0200

    [FIX] service

--- apps/api/src/user/traveler/traveler.service.ts
+++ apps/api/src/user/traveler/traveler.service.ts
@@ -3,48 +3,55 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
-} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ApiLimitResourceQuery } from '@travel-tailor/types';
-import { Repository } from 'typeorm';
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ApiLimitResourceQuery } from '@travel-tailor/types'
+import { Repository } from 'typeorm'
 
-import { CreateTravelerDto } from './dto/create-traveler.dto';
-import { UpdateTravelerDTO } from './dto/update-traveler.dto';
-import { Traveler } from './entities/traveler.entity';
+import { CreateTravelerDto } from './dto/create-traveler.dto'
+import { UpdateTravelerDTO } from './dto/update-traveler.dto'
+import { Traveler } from './entities/traveler.entity'
+import { CustomerService } from '../../payment/customer/customer.service'
 
 @Injectable()
 export class TravelerService {
   constructor(
     @InjectRepository(Traveler)
     private travelerRepository: Repository<Traveler>,
+    private customerService: CustomerService,
   ) {}
 
   async create(createTravelerDto: CreateTravelerDto) {
     try {
-      return await this.travelerRepository.save(createTravelerDto);
+      const customer = await this.customerService.create(createTravelerDto);
+      const traveler = this.travelerRepository.create({...createTravelerDto, customer})
+
+      return await this.travelerRepository.save(traveler)
     } catch (error) {
-      throw new BadRequestException(error);
+      throw new BadRequestException(error)
     }
   }
 
   async findAll(queries: ApiLimitResourceQuery) {
     try {
-      let { page, limit } = queries;
-      page = page ? +page : 1;
-      limit = limit ? +limit : 10;
+      let { page, limit } = queries
+      page = page ? +page : 1
+      limit = limit ? +limit : 10
 
       return await this.travelerRepository
         .createQueryBuilder('traveler')
         .leftJoinAndSelect('traveler.user', 'user')
+        .leftJoinAndSelect('traveler.customer', 'customer')
+        .leftJoinAndSelect('customer.orders', 'orders')
         .leftJoinAndSelect('traveler.tastes', 'tastes')
         .leftJoinAndSelect('traveler.travels', 'travels')
         .leftJoinAndSelect('traveler.comments', 'comments')
         .orderBy('traveler.createdAt', 'DESC')
         .skip((page - 1) * limit)
         .take(limit)
-        .getMany();
+        .getMany()
     } catch (error) {
-      throw new NotFoundException(error);
+      throw new NotFoundException(error)
     }
   }
 
@@ -54,28 +61,30 @@ export class TravelerService {
         .createQueryBuilder('traveler')
         .where('traveler.id = :id', { id })
         .leftJoinAndSelect('traveler.user', 'user')
+        .leftJoinAndSelect('traveler.customer', 'customer')
+        .leftJoinAndSelect('customer.orders', 'orders')
         .leftJoinAndSelect('traveler.tastes', 'tastes')
         .leftJoinAndSelect('traveler.travels', 'travels')
         .leftJoinAndSelect('traveler.comments', 'comments')
-        .getOne();
+        .getOne()
     } catch (error) {
-      throw new NotFoundException(error);
+      throw new NotFoundException(error)
     }
   }
 
   async update(id: string, updateTravelerDto: UpdateTravelerDTO) {
     try {
-    return await this.travelerRepository.update(id, updateTravelerDto);
+      return await this.travelerRepository.update(id, updateTravelerDto)
     } catch (error) {
-      throw new UnauthorizedException(error);
+      throw new UnauthorizedException(error)
     }
   }
 
   async remove(id: string) {
     try {
-      return await this.travelerRepository.softDelete(id);
+      return await this.travelerRepository.softDelete(id)
     } catch (error) {
-      throw new UnauthorizedException(error);
+      throw new UnauthorizedException(error)
     }
   }
 }
