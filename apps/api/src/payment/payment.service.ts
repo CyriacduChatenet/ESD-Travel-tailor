143fd775d61f7fa3d1de8c43b080a73db9e71418
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-04-05 19:52:29 +0200

    [FIX] payment status code

--- apps/api/src/payment/payment.service.ts
+++ apps/api/src/payment/payment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, Injectable } from '@nestjs/common'
 import { InjectStripe } from 'nestjs-stripe'
 import Stripe from 'stripe'
 import { ConfigService } from '@nestjs/config'
@@ -13,25 +13,29 @@ export class PaymentService {
   ) {}
 
   async createCheckoutSession(createCheckoutDto: CreateCheckoutDto): Promise<string> {
-    const session = await this.stripeClient.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: createCheckoutDto.currency,
-            product_data: {
-              name: 'Product Name',
-              description: 'Product Description',
+    try {
+      const session = await this.stripeClient.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: createCheckoutDto.currency,
+              product_data: {
+                name: 'Product Name',
+                description: 'Product Description',
+              },
+              unit_amount: createCheckoutDto.amount * 100,
             },
-            unit_amount: createCheckoutDto.amount * 100,
+            quantity: 1,
           },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `${this.configService.get('CLIENT_APP_URL')}/payment/success`,
-      cancel_url: `${this.configService.get('CLIENT_APP_URL')}/payment/cancel`,
-    });
-    return session.id;
+        ],
+        mode: 'payment',
+        success_url: `${this.configService.get('CLIENT_APP_URL')}/payment/success`,
+        cancel_url: `${this.configService.get('CLIENT_APP_URL')}/payment/cancel`,
+      });
+      return session.id;
+    } catch (err) {
+      throw new HttpException(err.message, 402);
+    }
   }
 }
