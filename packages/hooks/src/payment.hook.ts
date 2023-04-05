6aee0d438a9eb1253124dcdfe9648e315d9fbf7f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-04-05 20:08:57 +0200

    [FIX] payment hook

--- packages/hooks/src/payment.hook.ts
+++ packages/hooks/src/payment.hook.ts
@@ -1,23 +1,17 @@
 import { Stripe } from '@stripe/stripe-js';
 import { API_PAYMENT_CHECKOUT_ROUTE } from '@travel-tailor/constants';
+import { useFetch } from './fetch.hook'
 
 export const usePayment = async (api_url: string, stripePromise: Promise<Stripe | null>, credentials: {location: string, amount: number}) => {
-    const response = await fetch(`${api_url}${API_PAYMENT_CHECKOUT_ROUTE}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        location:  credentials.location,
-        amount: credentials.amount,
-      }),
-    })
-    const data = await response.json()
+  const response: any = useFetch.post(`${api_url}${API_PAYMENT_CHECKOUT_ROUTE}`, {
+    location:  credentials.location,
+    amount: credentials.amount,
+  })
 
     const stripe = await stripePromise
     if (stripe) {
       const result = await stripe.redirectToCheckout({
-        sessionId: data.sessionId,
+        sessionId: response.sessionId,
       })
       if (result.error) {
         console.error(result.error.message)
