ea0f5ea9684a0169202a18baad34d95944dc1f4a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-04-17 21:01:03 +0200

    [FIX] home page service

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,22 +1,17 @@
-import { HelloService } from '@travel-tailor/services';
 import { WebCreateTravelForm } from '@travel-tailor/ui';
 import { NextPage } from 'next';
 
 import { Layout } from '@/layout';
 import { useUser } from '@travel-tailor/contexts';
 import { ROLES } from '@travel-tailor/constants';
-import { useState } from 'react';
 
-interface IProps {
-  message: string;
-}
 
-const Home: NextPage<IProps> = ({ message }) => {
+const Home: NextPage = () => {
   const { user } = useUser();
   return (
     <Layout>
       <>
-        <p>{message}</p>
+      <h1>Travel tailor app</h1>
         <br />
         { user.roles.includes(ROLES.TRAVELER) ? <WebCreateTravelForm api_url={`${process.env.NEXT_PUBLIC_API_URL}`} mapboxAccessToken={`${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}/> : null}
       </>
@@ -25,16 +20,3 @@ const Home: NextPage<IProps> = ({ message }) => {
 };
 
 export default Home;
-
-export const getServerSideProps = async () => {
-  const error = {};
-  const response = await HelloService.getHello(
-    `${process.env.NEXT_PUBLIC_API_URL}`,
-  );
-  return {
-    props: {
-      message: response.message,
-      error
-    },
-  };
-};
