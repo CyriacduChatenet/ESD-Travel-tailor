516a78606604ac09a41ae2c701bc7a87b9125761
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-04 15:01:46 +0200

    [FEAT] tests

---
+++ apps/client/cypress/e2e/comment.spec.cy.ts
@@ -0,0 +1,7 @@
+describe('Comment', () => {
+    beforeEach(() => {
+        cy.login('test1@test.com','root')
+    });
+
+    it('should create a comment', () => {});
+});
--- apps/client/cypress/e2e/signup-user.spec.cy.ts
+++ apps/client/cypress/e2e/signup-user.spec.cy.ts
@@ -16,16 +16,16 @@ describe('Signup user', () => {
         cy.get('#create-advertiser-form').submit();
     });
 
-    it('should create a traveler', () => {
-        const number = Math.floor(Math.random() * 1000000);
+    // it('should create a traveler', () => {
+    //     const number = Math.floor(Math.random() * 1000000);
         
-        cy.get('#username-signup-input').type(`test${number}`);
-        cy.get('#email-signup-input').type(`test${number}@test.com`);
-        cy.get('#password-signup-input').type('root');
-        cy.get('#role-selector').select('traveler');
-        cy.get('#signup-form').submit();
-        cy.get('#create-taste-name').type(`Patinoire`);
-        cy.get('#create-taste-form').submit();
-        cy.get('#add-taste').click();
-    });
+    //     cy.get('#username-signup-input').type(`test${number}`);
+    //     cy.get('#email-signup-input').type(`test${number}@test.com`);
+    //     cy.get('#password-signup-input').type('root');
+    //     cy.get('#role-selector').select('traveler');
+    //     cy.get('#signup-form').submit();
+    //     cy.get('#create-taste-name').type(`Patinoire`);
+    //     cy.get('#create-taste-form').submit();
+    //     cy.get('#add-taste').click();
+    // });
 });
