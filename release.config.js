ed4c2c8bbc3d212f49760c10941343f4537c05fd
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-08 21:11:15 +0200

    [FIX] files

--- release.config.js
+++ release.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  branches: ["main"],
+  repositoryUrl: 'https://github.com/CyriacduChatenet/Travel-tailor',
+  branches: ['main'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/git",
+    "@semantic-release/commit-analyzer", 
+    "@semantic-release/github",
+    ["@semantic-release/npm", {
+      npmPublish: false
+    }],
+    ["@semantic-release/release-notes-generator"],
+    ["@semantic-release/changelog"],
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "[CHORE](release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
   ],
-  repositoryUrl: "https://github.com/CyriacduChatenet/Travel-tailor.git",
-};
+}
