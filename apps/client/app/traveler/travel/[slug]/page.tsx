4c8adfa4caecae2e6be3154d769f57c217c0f49a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-10 17:59:50 +0200

    feat(client/routes): add pages

---
+++ apps/client/app/traveler/travel/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const TravelerTravelPage: NextPage = () => {
+    return (
+        <main>
+            <h1>Traveler Travel</h1>
+        </main>
+    );
+};
+
+export default TravelerTravelPage;
