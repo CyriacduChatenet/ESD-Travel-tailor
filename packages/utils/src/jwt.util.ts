f702e5e9d0b5e22d9c51d3c88b2548f39b703291
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-11 16:10:37 +0200

    fix(utils/auth): change jwt util

--- packages/utils/src/jwt.util.ts
+++ packages/utils/src/jwt.util.ts
@@ -3,11 +3,31 @@ import { AccessToken } from '@travel-tailor/types';
 import { ROUTES } from '@travel-tailor/constants';
 import { TokenService } from '@travel-tailor/services'
 
-export const checkJwtValidity = (token: string) => {
-    const decodedToken: AccessToken = jwtDecode(token);
+export const checkJwtValidity = () => {
+    const token = TokenService.getAccessToken();
 
-    if(decodedToken.exp < Date.now()) {
-        TokenService.removeAccessToken();
-        window.location.pathname = ROUTES.AUTH.SIGNIN;
+  if (token) {
+    try {
+      const decodedToken = jwtDecode(token) as { exp: number };
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp < currentTime) {
+        // Token has expired
+        console.log('Token has expired');
+        // Vous pouvez ajouter ici le code pour gérer le cas où le token a expiré
+      } else {
+        // Token is still valid
+        console.log('Token is still valid');
+        // Vous pouvez ajouter ici le code pour gérer le cas où le token est valide
+      }
+    } catch (error) {
+      // Token is invalid
+      console.log('Invalid token');
+      // Vous pouvez ajouter ici le code pour gérer le cas où le token est invalide
     }
+  } else {
+    // Token is not present in local storage
+    console.log('Token not present in local storage');
+    // Vous pouvez ajouter ici le code pour gérer le cas où le token n'est pas présent dans le local storage
+  }
 };
