f78e8ca4084dfc1aecb45c8a766d64b638f352dc
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-11 18:06:32 +0200

    feat(client/navigation): add create taste page

---
+++ apps/client/app/traveler/taste/create/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const CreateTastePage: NextPage = () => {
+    return (
+        <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
+            <h1>Create Taste Page</h1>
+        </main>
+    );
+}
+
+export default CreateTastePage;
