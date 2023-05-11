9096d77599aac58268a710b717dcc2b44c59b859
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-11 11:36:57 +0200

    fix(client/routes): remove pages

---
+++ apps/client/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const NotFoundPage: NextPage = () => {
+    return (
+        <main>
+            <h1>404 not found</h1>
+        </main>
+    );
+};
+
+export default NotFoundPage;
