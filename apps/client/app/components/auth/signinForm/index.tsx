097ff026b156f4e714cc78448485440330fdbe1d
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-11 15:15:53 +0200

    feat(client/auth): add signin form

---
+++ apps/client/app/components/auth/signinForm/index.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Link from "next/link";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+
+export const SigninForm: FC = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data: any) => {
+        console.log(data)
+    };
+
+    console.log(errors);
+
+    return (
+        <div className="max-w-md mx-auto mt-4 col-span-4 md:col-span-8 xl:col-span-12">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+                        Email
+                    </label>
+                    <input
+                        {...register("email", {
+                            required: "Email is required",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Email is invalid",
+                            },
+                        })}
+                        id="email"
+                        type="email"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                    {errors.email && <p className="mt-2 text-red-500 text-xs italic">{errors.email.message?.toString()}</p>}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+                        Password
+                    </label>
+                    <input
+                        {...register("password", { required: "Password is required" })}
+                        id="password"
+                        type="password"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                    {errors.password && <p className="mt-2 text-red-500 text-xs italic">{errors.password.message?.toString()}</p>}
+                </div>
+                <div className="flex flex-col items-center justify-between">
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                        Signin
+                    </button>
+                    <Link href="/signup" className="mt-8 text-sm text-gray-500 hover:text-gray-800">
+                        Signup
+                    </Link>
+                </div>
+            </form>
+        </div>
+
+    );
+};
--- apps/client/app/signin/page.tsx
+++ apps/client/app/signin/page.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
 
+import { SigninForm } from "../components/auth/signinForm";
+
 const SigninPage: NextPage = () => {
     return (
-        <main>
-            <h1>Signin</h1>
+        <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
+            <h1 className="col-span-4 md:col-span-8 xl:col-span-12 text-center">Signin</h1>
+            <SigninForm />
         </main>
     );
 };
