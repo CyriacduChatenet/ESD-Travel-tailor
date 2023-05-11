0be88f5cafe2668393b213c157bb86a3544fb521
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-11 16:28:56 +0200

    fix(client/config): nextjs config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    transpilePackages: [
+        '@travel-tailor/functions',
+        '@travel-tailor/types',
+        '@travel-tailor/hooks',
+        '@travel-tailor/contexts',
+        '@travel-tailor/services',
+        '@travel-tailor/constants',
+        '@travel-tailor/ui',
+      ],
+      images: {
+        domains: ['images.unsplash.com'],
+    },
+    
+}
 
 module.exports = nextConfig
