4793f032de097d8c30b40863cb6d0070a84595b6
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-12 23:25:57 +0200

    fix(client/packages): change localStorage import

--- packages/services/src/token.service.ts
+++ packages/services/src/token.service.ts
@@ -1,27 +1,27 @@
 import { ACCESS_TOKEN, SIGNIN_TOKEN } from '@travel-tailor/constants'
 
 const getAccessToken = () => {
-  return localStorage.getItem(ACCESS_TOKEN)
+  return window.localStorage.getItem(ACCESS_TOKEN)
 }
 
 const setAccessToken = (accessToken: string) => {
-  localStorage.setItem(ACCESS_TOKEN, accessToken)
+  window.localStorage.setItem(ACCESS_TOKEN, accessToken)
 }
 
 const removeAccessToken = () => {
-  localStorage.removeItem(ACCESS_TOKEN)
+  window.localStorage.removeItem(ACCESS_TOKEN)
 }
 
 const getSigninToken = () => {
-  return localStorage.getItem(SIGNIN_TOKEN)
+  return window.localStorage.getItem(SIGNIN_TOKEN)
 }
 
 const setSigninToken = (signinToken: string) => {
-  localStorage.setItem(SIGNIN_TOKEN, signinToken)
+  window.localStorage.setItem(SIGNIN_TOKEN, signinToken)
 }
 
 const removeSigninToken = () => {
-  localStorage.removeItem(SIGNIN_TOKEN)
+  window.localStorage.removeItem(SIGNIN_TOKEN)
 }
 
 export const TokenService = {
