c63a044967b6e2b34e93831c1c2eae7e2beb1211
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-12 11:53:25 +0200

    feat(lient/payment): add payment pages

--- apps/client/app/payment/success/page.tsx
+++ apps/client/app/payment/success/page.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const PaymentSuccessPage: NextPage = () => {
     return (
-        <main>
+        <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
             <h1>Payment Success</h1>
         </main>
     );
