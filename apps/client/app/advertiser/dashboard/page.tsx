0bd1a9c375934e2a1c4bca0ac52c0b3f7ca6a2b7
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-12 11:55:23 +0200

    fix(client/pages): add paddings

--- apps/client/app/advertiser/dashboard/page.tsx
+++ apps/client/app/advertiser/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { AuthChecker } from "@/components/auth/authChecker";
 const AdvertiserDashboardPage: NextPage = () => {
     return (
         <AuthChecker>
-            <main>
+            <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
                 <h1>Advertiser Dashboard</h1>
             </main>
         </AuthChecker>
--- apps/client/app/traveler/dashboard/page.tsx
+++ apps/client/app/traveler/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { AuthChecker } from "@/components/auth/authChecker";
 const TravelerDashboardPage: NextPage = () => {
     return (
         <AuthChecker>
-            <main>
+            <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
                 <h1>Traveler Dashboard</h1>
             </main>
         </AuthChecker>
