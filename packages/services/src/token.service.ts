e2c260886f0dc1657eda82a11cd6d2c176b33446
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-12 23:34:35 +0200

    fix(cclient/localStorage): add window type checker

--- packages/services/src/token.service.ts
+++ packages/services/src/token.service.ts
@@ -1,27 +1,39 @@
 import { ACCESS_TOKEN, SIGNIN_TOKEN } from '@travel-tailor/constants'
 
 const getAccessToken = () => {
-  return window.localStorage.getItem(ACCESS_TOKEN)
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(ACCESS_TOKEN)
+  }
 }
 
 const setAccessToken = (accessToken: string) => {
-  window.localStorage.setItem(ACCESS_TOKEN, accessToken)
+  if (typeof window !== "undefined") {
+    return localStorage.setItem(ACCESS_TOKEN, accessToken)
+  }
 }
 
 const removeAccessToken = () => {
-  window.localStorage.removeItem(ACCESS_TOKEN)
+  if (typeof window !== "undefined") {
+    return localStorage.removeItem(ACCESS_TOKEN)
+  }
 }
 
 const getSigninToken = () => {
-  return window.localStorage.getItem(SIGNIN_TOKEN)
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(SIGNIN_TOKEN)
+  }
 }
 
 const setSigninToken = (signinToken: string) => {
-  window.localStorage.setItem(SIGNIN_TOKEN, signinToken)
+  if (typeof window !== "undefined") {
+    return localStorage.setItem(SIGNIN_TOKEN, signinToken)
+  }
 }
 
 const removeSigninToken = () => {
-  window.localStorage.removeItem(SIGNIN_TOKEN)
+  if (typeof window !== "undefined") {
+    return localStorage.removeItem(SIGNIN_TOKEN)
+  }
 }
 
 export const TokenService = {
