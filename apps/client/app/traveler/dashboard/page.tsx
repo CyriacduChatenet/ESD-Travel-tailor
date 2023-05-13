444e5002a9fcc91641d24a1858ea175687f30120
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-13 16:54:06 +0200

    feat(client/map): add mapbox

--- apps/client/app/traveler/dashboard/page.tsx
+++ apps/client/app/traveler/dashboard/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import React from "react";
 import { NextPage } from "next";
 
 import { AuthChecker } from "@/components/auth/authChecker";
 import { TravelList } from "@/components/traveler/travels/travelList";
+import { Mapbox } from "@/components/map";
 
 const TravelerDashboardPage: NextPage = () => {
     return (
@@ -12,10 +14,13 @@ const TravelerDashboardPage: NextPage = () => {
                     <h1 className="font-bold lg:text-2xl">Traveler Dashboard</h1>
                     <section className="grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
                         <div className="col-span-4 md:col-span-4 xl:col-span-8">
-                            <TravelList/>
+                            <TravelList />
                         </div>
                         <div className="col-span-4 md:col-span-4 xl:col-span-4">
-                            <p>map</p>
+                            <Mapbox
+                                mapboxApiAccessToken={`${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
+                                addresse={`Bordeaux, Gironde, France`}
+                            />
                         </div>
                     </section>
                 </section>
