2453c9cccc95ccb220672833bac8e7e3b8576fd7
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-16 14:13:14 +0200

    styles(client/traveler): add comment profile picture

--- apps/client/components/traveler/travels/activity/comments/comment/index.tsx
+++ apps/client/components/traveler/travels/activity/comments/comment/index.tsx
@@ -1,20 +1,24 @@
+import { Icon } from "@iconify/react";
 import Image from "next/image";
 import { FC } from "react";
 
 interface IProps {
     author: string;
+    role: string;
     content: string;
     createdAt: Date;
 }
 
-export const Comment: FC<IProps> = ({ author, content, createdAt }) => {
+export const Comment: FC<IProps> = ({ author, role, content, createdAt }) => {
     return (
         <li className="shadow appearance-none border rounded w-full py-4 px-6 my-4 text-gray-700 leading-tight">
             <div className="flex justify-between items-center w-full">
-                <Image src={''} alt="" width={32} height={32} />
+                <div className="flex justify-center items-center w-8 h-8 rounded-full bg-gray-400 text-white">
+                    <Icon icon="material-symbols:person" className="w-6 h-6" />
+                </div>
                 <p className="w-full flex justify-around items-center">
                     <span className="flex justify-around items-center w-1/3">
-                        <span>{author}</span><div className="hidden lg:block"><span>Role</span></div>
+                        <span className="font-bold">{author}</span><div className="hidden lg:block font-normal italic"><span>{role}</span></div>
                     </span>
                     <span>{new Date(createdAt).toLocaleString('fr')}</span>
                 </p>
