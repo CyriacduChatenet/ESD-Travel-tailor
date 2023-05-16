8ca5f906d7561c7268fa45c8b3f03493be9eb665
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-16 23:24:43 +0200

    styles(client/advertiser): change create activity banner

--- apps/client/pages/advertiser/activity/create/index.tsx
+++ apps/client/pages/advertiser/activity/create/index.tsx
@@ -12,15 +12,17 @@ const AdvertiserCreateActivityPage: NextPage = () => {
         <AuthChecker>
             <Layout>
                 <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12">
-                    <h1>Advertiser Create Activity</h1>
-                    <Link href={ROUTES.ADVERTISER.DASHBOARD}>
-                        <button
-                            type="submit"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        >
-                            Return
-                        </button>
-                    </Link>
+                    <div className="col-span-4 md:col-span-8 xl:col-span-12 flex items-center justify-around">
+                        <Link href={ROUTES.ADVERTISER.DASHBOARD}>
+                            <button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            >
+                                Return
+                            </button>
+                        </Link>
+                        <h1>Advertiser Create Activity</h1>
+                    </div>
                     <CreateActivityForm />
                 </main>
             </Layout>
