f2236c05a1dd5b1694ebe4e59b36d4ae63862405
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-16 11:53:00 +0200

    fix(client/type): change comment type

--- apps/client/components/traveler/travels/activity/comments/commentList/index.tsx
+++ apps/client/components/traveler/travels/activity/comments/commentList/index.tsx
@@ -10,7 +10,7 @@ export const CommentList: FC<IProps> = ({ comments }) => {
     console.log(comments);
     return (
         <ul>
-            {comments && comments.map((comment) => <Comment key={comment.id} author={'author'} content={comment.content} createdAt={comment.createdAt} />)}
+            {comments && comments.map((comment: any) => <Comment key={comment.id} author={'author'} content={comment.content} createdAt={comment.createdAt} />)}
         </ul>
     );
 };
