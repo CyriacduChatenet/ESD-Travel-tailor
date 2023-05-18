a63e100a42ea7640b27ef04245dbbe11273ac7d9
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-18 11:22:00 +0200

    fix(client/utils): remove unused utils

--- packages/utils/src/city-regex.util.ts
+++
@@ -1,4 +0,0 @@
-export const testCityUtil = (value: string) => {
-    const cityRegex = new RegExp(/^[a-zA-Z\u0080-\u024F]+(?:([\ \-\']|(\.\ ))[a-zA-Z\u0080-\u024F]+)*$/)
-    return cityRegex.test(value)
-}
