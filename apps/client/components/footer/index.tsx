fa3f86571c7fbd16a039e34fdccb8d3dab9867cd
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-18 12:41:32 +0200

    fix(client/layout): change footer

--- apps/client/components/footer/index.tsx
+++ apps/client/components/footer/index.tsx
@@ -1,24 +1,24 @@
+import { ROUTES } from "@travel-tailor/constants";
 import { Icon } from "@iconify/react";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 export const Footer: FC = () => {
-    const [date, setDate] = useState(new Date());
     return (
         <footer className="md:grid md:grid-cols-8 xl:grid-cols-12 flex flex-col-reverse justify-around px-10 py-8 bg-blue-500 text-white">
             <div className="col-span-4 md:col-span-3 lg:col-span-4 flex justify-center lg:justify-around md:items-center">
                 <p className="hidden xl:visible">Travel Tailor</p>
-                <p>© {date.getFullYear()} Travel Tailor, Inc</p>
+                <p>© {new Date().getFullYear()} Travel Tailor, Inc</p>
             </div>
             <ul className="col-span-4 md:col-span-3 lg:col-span-4 flex flex-col lg:flex-row items-center lg:justify-around my-8 md:my-0">
                 <li className="my-2">
-                    <Link href={''}>Site map</Link>
+                    <Link href={ROUTES.SITE_MAP}>Site map</Link>
                 </li>
                 <li className="my-2">
-                    <Link href={''}>General conditions</Link>
+                    <Link href={ROUTES.TERMS_AND_CONDITIONS}>Terms And Conditions</Link>
                 </li>
                 <li className="my-2">
-                    <Link href={''}>About project</Link>
+                    <Link href={ROUTES.ABOUT_US}>About project</Link>
                 </li>
             </ul>
             <div className="col-span-4 md:col-span-2 lg:col-span-4 flex justify-center items-center">
