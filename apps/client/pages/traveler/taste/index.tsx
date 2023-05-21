60495ec06b5352352851e7d0586ea5ad1d78088b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-21 22:08:14 +0200

    fix(client/layout): fix seo

--- apps/client/pages/traveler/taste/index.tsx
+++ apps/client/pages/traveler/taste/index.tsx
@@ -14,7 +14,7 @@ const TastesPage: NextPage = () => {
 
     return (
         <AuthChecker>
-            <Layout>
+            <Layout title={"Traveler Tastes"} description={" Traveler Taste"}>
                 <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12 pt-20">
                     <section className="col-span-4 md:col-span-8 xl:col-span-12 pt-4 md:pt-8">
                         <h1 className="font-bold lg:text-2xl">Tastes</h1>
