b4227dd1af2d5cd436bf4457add7b73321257406
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-23 20:12:41 +0200

    fix(api/repository): add repository

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -4,24 +4,23 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
 import { ApiLimitResourceQuery } from '@travel-tailor/types'
-import { DeleteResult, Repository } from 'typeorm'
+import { DeleteResult } from 'typeorm'
 
 import { SignupUserInputDTO } from './dto/signup-user.dto'
 import { UpdateUserDTO } from './dto/update-user.dto'
 import { User } from './entities/user.entity'
 import { testEmailUtil } from '../config/utils/regex-test-email.util'
+import { UserRepository } from './user.repository'
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User) private userRepository: Repository<User>
-  ) {}
+  constructor(private userRepository: UserRepository) {}
+
   async create(signupUserDto: SignupUserInputDTO): Promise<User> {
     try {
       if(testEmailUtil(signupUserDto.email)) {
-        return await this.userRepository.save(signupUserDto)
+        return await this.userRepository.createUser(signupUserDto)
       } else {
         throw new BadRequestException('email must contain ***@***.***')
       }
@@ -32,43 +31,7 @@ export class UserService {
 
   async findAll(queries: ApiLimitResourceQuery) {
     try {
-      let { page, limit, sortedBy, username, email, roles } = queries;
-      page = page ? +page : 1;
-      limit = limit ? +limit : 10;
-
-      const query = this.userRepository
-        .createQueryBuilder('user')
-        .leftJoinAndSelect('user.traveler', 'traveler')
-        .leftJoinAndSelect('traveler.tastes', 'tastes')
-        .leftJoinAndSelect('traveler.travels', 'travels')
-        .leftJoinAndSelect('traveler.customer', 'customer')
-        .leftJoinAndSelect('user.advertiser', 'advertiser')
-        .leftJoinAndSelect('user.resetPasswordToken', 'resetPasswordToken')
-
-      if (sortedBy) {
-        query.orderBy('user.createdAt', sortedBy)
-      } else {
-        query.orderBy('user.createdAt', 'DESC')
-      }
-
-      if (username) {
-        query.andWhere('user.username LIKE :username', { username })
-      }
-
-      if (email) {
-        query.andWhere('user.email LIKE :email', { email })
-      }
-
-      if (roles) {
-        query.andWhere('user.roles = :roles', { roles })
-      }
-      
-        return {
-          page: page,
-          limit: limit,
-          total: await query.getCount(),
-          data: await query.skip((page - 1) * limit).take(limit).getMany()
-        }
+      return await this.userRepository.findAllUser(queries)
     } catch (error) {
       throw new NotFoundException(error)
     }
@@ -76,16 +39,7 @@ export class UserService {
 
   public async findOneByEmail(email: string): Promise<User> {
     try {
-      return await this.userRepository
-        .createQueryBuilder('user')
-        .where('user.email = :email', { email })
-        .leftJoinAndSelect('user.traveler', 'traveler')
-        .leftJoinAndSelect('traveler.travels', 'travels')
-        .leftJoinAndSelect('traveler.tastes', 'tastes')
-        .leftJoinAndSelect('user.advertiser', 'advertiser')
-        .leftJoinAndSelect('advertiser.activities', 'activities')
-        .leftJoinAndSelect('user.resetPasswordToken', 'resetPasswordToken')
-        .getOne()
+      return await this.userRepository.findOneUserByEmail(email)
     } catch (error) {
       throw new NotFoundException(error)
     }
@@ -93,7 +47,7 @@ export class UserService {
 
   async update(id: string, signupUserDto: UpdateUserDTO | unknown) {
     try {
-      return await this.userRepository.update(id, signupUserDto)
+      return await this.userRepository.updateUser(id, signupUserDto)
     } catch (error) {
       throw new BadRequestException(error)
     }
@@ -101,7 +55,7 @@ export class UserService {
 
   async remove(id: string): Promise<DeleteResult> {
     try {
-      return await this.userRepository.softDelete(id)
+      return await this.userRepository.removeUser(id)
     } catch (error) {
       throw new UnauthorizedException(error)
     }
