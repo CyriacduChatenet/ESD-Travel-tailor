884d4b0401ddb4eadc402d4fb003d34c33a67e0a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-25 10:17:45 +0200

    fix(api/payment): change stripe config

--- apps/api/src/payment/payment.module.ts
+++ apps/api/src/payment/payment.module.ts
@@ -1,6 +1,8 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { StripeModule } from 'nestjs-stripe';
+import { forwardRef, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { StripeModule } from '@golevelup/nestjs-stripe';
+import { applyRawBodyOnlyTo } from '@golevelup/nestjs-webhooks';
 import { ConfigModule } from '@nestjs/config';
+import { SkipThrottle } from '@nestjs/throttler';
 
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
@@ -10,22 +12,35 @@ import { StripeCustomerService } from './stripe-customer.service';
 import { OpencageModule } from '../opencage/opencage.module';
 import { MailModule } from '../mail/mail.module';
 import { StripeWebhookService } from './stripe-webhook.service';
-import { StripeWebhookController } from './stripe-webhook.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    StripeModule.forRoot({
-      apiKey: process.env.STRIPE_API_KEY,
+    StripeModule.forRoot(StripeModule, {
+      apiKey: process.env.STRIPE_SECRET_KEY,
       apiVersion: '2022-11-15',
+      webhookConfig: {
+        stripeSecrets: {
+          account: process.env.STRIPE_WEBHOOK_SECRET,
+          connect: process.env.STRIPE_WEBHOOK_SECRET,
+        },
+        decorators: [SkipThrottle()],
+      },
     }),
     MailModule,
     OrderModule,
     forwardRef(() => CustomerModule),
     OpencageModule,
   ],
-  controllers: [PaymentController, StripeWebhookController],
+  controllers: [PaymentController],
   providers: [PaymentService, StripeCustomerService, StripeWebhookService],
   exports: [PaymentService, StripeCustomerService],
 })
-export class PaymentModule {}
+export class PaymentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    applyRawBodyOnlyTo(consumer, {
+      method: RequestMethod.ALL,
+      path: 'stripe/webhook',
+    });
+  }
+}
--- apps/api/src/payment/stripe-webhook.service.ts
+++ apps/api/src/payment/stripe-webhook.service.ts
@@ -1,5 +1,5 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { InjectStripe } from "nestjs-stripe";
+import { BadRequestException, HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectStripeModuleConfig, StripeWebhookHandler } from "@golevelup/nestjs-stripe";
 import Stripe from "stripe";
 import { ConfigService } from "@nestjs/config";
 
@@ -9,41 +9,19 @@ import { StripeCustomerService } from "./stripe-customer.service";
 @Injectable()
 export class StripeWebhookService {
   constructor(
-    @InjectStripe() private readonly stripeClient: Stripe,
+    @InjectStripeModuleConfig() private readonly stripeClient: Stripe,
     private configService: ConfigService,
     private mailService: MailService,
     private stripeCustomerService: StripeCustomerService,
-    ) {}
+  ) { }
 
-  constructEvent(body, headers, secret: string): Stripe.Event {
-    const signature = headers['stripe-signature'];
-
+  @StripeWebhookHandler('payment_intent.created')
+  async handleStripeWebhook(event: Stripe.Event) {
     try {
-      return this.stripeClient.webhooks.constructEvent(body, signature, secret);
-    } catch (err) {
-      console.error(`Error when build Stripe event: ${err}`);
-      throw new BadRequestException('Error when build Stripe event');
+      console.log('payment_intent.created', event)
+      return { success_payment: event }
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.PAYMENT_REQUIRED)
     }
-  }
-
-  async paymentWebhook(body) {
-    const event = this.constructEvent(body, body.headers, this.configService.get('STRIPE_WEBHOOK_SECRET'));
-    switch (event.type) {
-      case 'checkout.session.completed':
-        const session = event.data.object;
-        console.log(session);
-        // Fulfill the purchase...
-        // this.fulfillOrder(session);
-        break;
-      case 'invoice.paid':
-        const invoice = event.data.object;
-        console.log(invoice);
-        // this.fulfillOrder(session);
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-    return { received: true };
   }
 }
