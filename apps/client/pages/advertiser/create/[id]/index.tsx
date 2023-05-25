cacbe9939dc589bc7fbfb80a346dee3babf23512
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-05-25 11:47:02 +0200

    fix(files): resolve bugs

--- apps/client/pages/advertiser/create/[id]/index.tsx
+++ apps/client/pages/advertiser/create/[id]/index.tsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 
 import { Layout } from "@/components/layout";
 import { CreateAdvertiserForm } from "@/components/advertiser/createForm";
+import { parse } from "cookie";
 
-const CreateAdvertiserPage: NextPage = () => {
+interface IProps {
+    token : string
+}
+
+const CreateAdvertiserPage: NextPage<IProps> = ({ token }) => {
     return (
         <Layout title={""} description={""}>
             <main className="pt-20">
                 <h1>Create Advertiser</h1>
-                <CreateAdvertiserForm />
+                <CreateAdvertiserForm token={token} />
             </main>
         </Layout>
     );
 };
 
 export default CreateAdvertiserPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const error = {};
+
+    const cookies = req.headers.cookie;
+    const parsedCookies = cookies ? parse(cookies) : {};
+    const signinToken = parsedCookies.signinToken;
+    return {
+        props: {
+            token: signinToken
+        }
+    };
+};
