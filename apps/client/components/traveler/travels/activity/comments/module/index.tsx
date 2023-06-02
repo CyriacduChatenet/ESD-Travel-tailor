675be38191e53e1b6ca0e441be8b102cb1911c8a
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-02 11:39:04 +0200

    fix(client/activity): change comment ui

--- apps/client/components/traveler/travels/activity/comments/module/index.tsx
+++ apps/client/components/traveler/travels/activity/comments/module/index.tsx
@@ -1,7 +1,7 @@
 import { FC, Dispatch, SetStateAction } from "react";
 import { Activity, Comment, User } from "@travel-tailor/types";
 
-// import { CommentToolbar } from "@/components/traveler/travels/activity/comments/toolBar";
+import { CommentToolbar } from "@/components/traveler/travels/activity/comments/toolBar";
 import { CommentMark } from "@/components/traveler/travels/activity/comments/commentMark";
 import { CommentList } from "@/components/traveler/travels/activity/comments/commentList";
 import { CommentForm } from "@/components/traveler/travels/activity/comments/commentForm";
@@ -17,7 +17,7 @@ interface IProps {
 export const CommentModule: FC<IProps> = ({ setDisplayCommentModule, comments, data, setData, user }) => {
     return (
         <>
-        {/* <CommentToolbar setDisplayCommentModule={setDisplayCommentModule} /> */}
+        <CommentToolbar setDisplayCommentModule={setDisplayCommentModule} />
         <section className="grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12 gap-5 mb-8 xl:mb-20">
             <CommentMark commentsLength={comments.length} marks={data.marks} />
             <section className="col-span-8 md:col-span-4 xl:col-span-8">
