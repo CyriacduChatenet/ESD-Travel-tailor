d1080c1f35dd7cb9c45ad415229bdcd20e02315b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-05 14:54:01 +0200

    fix(api/planning): change update activity planning form

--- apps/api/src/user/traveler/travel/planning/planning.controller.ts
+++ apps/api/src/user/traveler/travel/planning/planning.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Param, Patch } from "@nestjs/common";
+import { Body, Controller, Param, Patch, UseGuards } from "@nestjs/common";
+import { Throttle, ThrottlerGuard } from "@nestjs/throttler";
 
 import { PlanningService } from "./planning.service";
-import { User } from "../../../../config/decorators/user.decorator";
+import { JwtAuthGuard } from "../../../../auth/guards/jwt-auth.guard";
+import { Role } from "../../../../config/enum/role.enum";
+import { Roles } from "../../../../config/decorators/roles.decorator";
+import { UpdatePlanningActivityDto } from "./dto/update-planning-activity.dto";
 
 @Controller("planning")
+@UseGuards(ThrottlerGuard)
 export class PlanningController {
     constructor(private readonly planningService: PlanningService) {}
 
+    @Throttle(100, 60)
+    @UseGuards(JwtAuthGuard)
+    @Roles(Role.Traveler, Role.Admin)
     @Patch(':travel_id')
-    async updatePlanning(@User() user, @Param('travel_id') travel_id: string) {
-        return await this.planningService.update(user, travel_id);
+    async updatePlanningActivity(@Param('travel_id') travel_id: string, @Body() updatePlanningActivityDto: UpdatePlanningActivityDto) {
+        return await this.planningService.updatePlanningActivity(travel_id, updatePlanningActivityDto);
     }
 }
