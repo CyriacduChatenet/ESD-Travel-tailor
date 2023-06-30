7898f8cff344997b413286531ac629e1982895ef
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 11:48:08 +0200

    doc(api): add activity detail doc

--- apps/api/src/activity/activity-detail/activity-detail.controller.ts
+++ apps/api/src/activity/activity-detail/activity-detail.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
+import { ApiTags, ApiOperation, ApiOkResponse, ApiCreatedResponse, ApiBadRequestResponse, ApiNotFoundResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { ApiLimitResourceQuery } from '@travel-tailor/types';
 
 import { Role } from '../../config/enum/role.enum';
@@ -21,6 +12,7 @@ import { UpdateActivityDetailDto } from './dto/update-activity-detail.dto';
 
 @Controller('activity-detail')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Activity Detail')
 export class ActivityDetailController {
   constructor(private readonly activityDetailService: ActivityDetailService) {}
 
@@ -28,18 +20,26 @@ export class ActivityDetailController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Advertiser, Role.Admin)
+  @ApiOperation({ summary: 'Create an activity detail' })
+  @ApiCreatedResponse({ description: 'Activity detail created successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
   create(@Body() createActivityDetailDto: CreateActivityDetailDto) {
     return this.activityDetailService.create(createActivityDetailDto);
   }
 
   @Get()
   @Throttle(1000, 60)
+  @ApiOperation({ summary: 'Get all activity details' })
+  @ApiOkResponse({ description: 'Successful operation' })
   findAll(@Query() queries: ApiLimitResourceQuery) {
     return this.activityDetailService.findAll(queries);
   }
 
   @Get(':id')
   @Throttle(1000, 60)
+  @ApiOperation({ summary: 'Get an activity detail by ID' })
+  @ApiOkResponse({ description: 'Successful operation' })
+  @ApiNotFoundResponse({ description: 'Activity detail not found' })
   findOne(@Param('id') id: string) {
     return this.activityDetailService.findOne(id);
   }
@@ -48,6 +48,10 @@ export class ActivityDetailController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Advertiser, Role.Admin)
+  @ApiOperation({ summary: 'Update an activity detail' })
+  @ApiOkResponse({ description: 'Activity detail updated successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   update(
     @Param('id') id: string,
     @Body() updateActivityDetailDto: UpdateActivityDetailDto,
@@ -59,6 +63,10 @@ export class ActivityDetailController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Advertiser, Role.Admin)
+  @ApiOperation({ summary: 'Delete an activity detail' })
+  @ApiOkResponse({ description: 'Activity detail deleted successfully' })
+  @ApiNotFoundResponse({ description: 'Activity detail not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   remove(@Param('id') id: string) {
     return this.activityDetailService.remove(id);
   }
--- apps/api/src/activity/activity-detail/dto/update-activity-detail.dto.ts
+++ apps/api/src/activity/activity-detail/dto/update-activity-detail.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UpdateActivityDetailDTO } from '@travel-tailor/types';
 import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateActivityDetailDto implements UpdateActivityDetailDTO {
   @IsNumber()
+  @ApiProperty({
+    description: 'Duration',
+    type: Number,
+    default: 1,
+    example: 1,
+  })
   duration: number;
 
   @IsString()
+  @ApiProperty({
+    description: 'Location',
+    type: String,
+    default: 'Bali',
+    example: 'Bali',
+  })
   location: string;
 }
