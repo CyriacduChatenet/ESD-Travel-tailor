bde215de677c0f3d0f9998e64314b74afe998458
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 16:06:35 +0200

    doc(api): add taste doc

--- apps/api/src/user/traveler/taste/dto/update-taste.dto.ts
+++ apps/api/src/user/traveler/taste/dto/update-taste.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UpdateTasteDTO } from '@travel-tailor/types';
 import { IsString } from 'class-validator';
 
 export class UpdateTasteDto implements UpdateTasteDTO {
   @IsString()
+  @ApiProperty({
+    description: 'The name of the taste',
+    type: String,
+    example: 'John Doe',
+  })
   name: string;
 }
--- apps/api/src/user/traveler/taste/taste.controller.ts
+++ apps/api/src/user/traveler/taste/taste.controller.ts
@@ -1,44 +1,43 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  Query,
-} from '@nestjs/common';
-import { ApiLimitResourceQuery } from '@travel-tailor/types';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
+import { ApiTags, ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiInternalServerErrorResponse } from '@nestjs/swagger';
 
 import { TasteService } from './taste.service';
 import { CreateTasteDto } from './dto/create-taste.dto';
 import { UpdateTasteDto } from './dto/update-taste.dto';
+import { ApiLimitResourceQuery } from '@travel-tailor/types';
 import { Roles } from '../../../config/decorators/roles.decorator';
 import { Role } from '../../../config/enum/role.enum';
 import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
 
 @Controller('taste')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Taste')
 export class TasteController {
   constructor(private readonly tasteService: TasteService) { }
 
   @Post()
   @Throttle(1000, 60)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ description: 'Taste created successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async create(@Body() createTasteDto: CreateTasteDto) {
     return this.tasteService.create(createTasteDto);
   }
 
   @Get()
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved all tastes successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findAll(@Query() queries: ApiLimitResourceQuery) {
     return this.tasteService.findAll(queries);
   }
 
   @Get(':id')
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved taste successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findOne(@Param('id') id: string) {
     return this.tasteService.findOne(id);
   }
@@ -47,6 +46,9 @@ export class TasteController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Updated taste successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async update(
     @Param('id') id: string,
     @Body() updateTasteDto: UpdateTasteDto,
@@ -58,6 +60,9 @@ export class TasteController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Deleted taste successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async remove(@Param('id') id: string) {
     return this.tasteService.remove(id);
   }
