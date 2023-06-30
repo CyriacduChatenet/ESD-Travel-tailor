8e458d9482d706a7e0182905e15ec646e7dd85c9
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 16:31:57 +0200

    doc(api): add user doc

--- apps/api/src/user/dto/login-user.dto.ts
+++ apps/api/src/user/dto/login-user.dto.ts
@@ -1,9 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class LoginUserInputDTO {
   @IsEmail()
+  @ApiProperty({
+    description: 'The email of the user',
+    type: String,
+    example: 'user@example.com',
+  })
+
   email: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'The password of the user',
+    type: String,
+    example: 'password',
+  })
   password: string;
 }
--- apps/api/src/user/dto/update-user.dto.ts
+++ apps/api/src/user/dto/update-user.dto.ts
@@ -2,23 +2,55 @@ import { IsObject, IsString } from 'class-validator';
 
 import { Advertiser } from '../advertiser/entities/advertiser.entity';
 import { Traveler } from '../traveler/entities/traveler.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { ResetPasswordToken } from 'src/auth/reset-password-token/entities/reset-password-token.entity';
 
 export class UpdateUserDTO {
   @IsString()
+  @ApiProperty({
+    description: 'The username of the user',
+    type: String,
+    example: 'John doe',
+  })
   username?: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'The password of the user',
+    type: String,
+    example: '********',
+  })
   password?: string;
 
   @IsString()
+  @ApiProperty({
+    description: 'The roles of the user',
+    type: String,
+    example: 'traveler',
+  })
   roles?: string;
 
   @IsObject()
+  @ApiProperty({
+    description: 'The advertiser of the user',
+    type: Advertiser,
+    example: 'advertiser',
+  })
   advertiser?: Advertiser;
 
   @IsObject()
+  @ApiProperty({
+    description: 'The traveler of the user',
+    type: Object,
+    example: Traveler,
+  })
   traveler?: Traveler;
 
   @IsObject()
+  @ApiProperty({
+    description: 'The reset password token of the user',
+    type: ResetPasswordToken,
+    example: 'resetPasswordToken',
+  })
   resetPasswordToken?: Object;
 }
