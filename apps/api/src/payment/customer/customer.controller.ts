f8e7e94602df0ce36fc8620c5ee03375d4c25793
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 15:47:16 +0200

    doc(api): add customer doc

--- apps/api/src/payment/customer/customer.controller.ts
+++ apps/api/src/payment/customer/customer.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiCreatedResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { ApiLimitResourceQuery } from '@travel-tailor/types';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
@@ -11,24 +12,31 @@ import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 
 @Controller('customer')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Customers')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Post()
   @Throttle(1000, 60)
   @Roles(Role.Traveler, Role.Advertiser, Role.Admin)
+  @ApiCreatedResponse({ description: 'Customer created successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
   async createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
     return await this.customerService.createCustomer(createCustomerDto);
   }
 
   @Get()
-  @Throttle(1000, 60)
+  @ApiCreatedResponse({ description: 'Customers found successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid customer ID' })
+  @Throttle(1000, 60)// Assuming you have defined this decorator to document the query parameters
   async findAll(@Query() queries: ApiLimitResourceQuery) {
     return await this.customerService.findAll(queries);
   }
 
   @Get(':id')
   @Throttle(1000, 60)
+  @ApiCreatedResponse({ description: 'Customer found successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid customer ID' })
   async findOne(@Param('id') id: string) {
     return await this.customerService.findOne(id);
   }
@@ -37,6 +45,8 @@ export class CustomerController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Advertiser, Role.Admin)
+  @ApiCreatedResponse({ description: 'Customer updated successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data or customer ID' })
   async update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
     return await this.customerService.update(id, updateCustomerDto);
   }
@@ -45,6 +55,8 @@ export class CustomerController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Advertiser, Role.Admin)
+  @ApiCreatedResponse({ description: 'Customer deleted successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid customer ID' })
   async remove(@Param('id') id: string) {
     return await this.customerService.remove(id);
   }
--- apps/api/src/payment/customer/dto/update-customer.dto.ts
+++ apps/api/src/payment/customer/dto/update-customer.dto.ts
@@ -1,12 +1,29 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class UpdateCustomerDto {
     @IsString()
+    @ApiProperty({
+        name: 'id',
+        description: 'The id of the customer',
+        type: String,
+        default: "cus_123456789",
+    })
     email: string;
 
     @IsString()
+    @ApiProperty({
+        description: 'The name of the customer',
+        type: String,
+        default: "John Doe",
+    })
     name: string;
 
     @IsString()
+    @ApiProperty({
+        description: 'The address of the customer',
+        type: String,
+        default: "1234 Main St",
+    })
     address: string;
 }
