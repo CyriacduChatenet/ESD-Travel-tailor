c12952a0a879748cdd4f33cdc5a06dc3c94a1d4b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 16:11:40 +0200

    doc(api): add timeslot doc

--- apps/api/src/user/traveler/travel/day/time-slot/dto/update-time-slot.dto.ts
+++ apps/api/src/user/traveler/travel/day/time-slot/dto/update-time-slot.dto.ts
@@ -2,18 +2,39 @@ import { IsDate, IsObject } from "class-validator";
 
 import { Activity } from "../../../../../../activity/entities/activity.entity";
 import { Day } from "../../entities/day.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class UpdateTimeSlotDto {
     @IsObject()
+    @ApiProperty({
+        description: 'The day of the time slot',
+        type: Day,
+        example: 'day',
+    })
     day?: Day;
 
     @IsObject()
+    @ApiProperty({
+        description: 'The activity of the time slot',
+        type: Activity,
+        example: 'activity',
+    })
     activity?: Activity;
 
     @IsDate()
+    @ApiProperty({
+        description: 'The start time of the time slot',
+        type: Date,
+        example: '09:00',
+    })
     startTime?: Date;
 
     @IsDate()
+    @ApiProperty({
+        description: 'The end time of the time slot',
+        type: Date,
+        example: '10:00',
+    })
     endTime?: Date;
 }
--- apps/api/src/user/traveler/travel/day/time-slot/time-slot.controller.ts
+++ apps/api/src/user/traveler/travel/day/time-slot/time-slot.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
-import { ApiLimitResourceQuery } from '@travel-tailor/types';
+import { ApiTags, ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiInternalServerErrorResponse } from '@nestjs/swagger';
 
 import { TimeSlotService } from './time-slot.service';
 import { CreateTimeSlotDto } from './dto/create-time-slot.dto';
 import { UpdateTimeSlotDto } from './dto/update-time-slot.dto';
+import { ApiLimitResourceQuery } from '@travel-tailor/types';
 import { JwtAuthGuard } from '../../../../../auth/guards/jwt-auth.guard';
 import { Roles } from '../../../../../config/decorators/roles.decorator';
 import { Role } from '../../../../../config/enum/role.enum';
 
 @Controller('time-slot')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Time Slot')
 export class TimeSlotController {
   constructor(private readonly timeSlotService: TimeSlotService) {}
 
@@ -18,18 +20,25 @@ export class TimeSlotController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ description: 'Time slot created successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async create(@Body() createTimeSlotDto: CreateTimeSlotDto) {
     return await this.timeSlotService.create(createTimeSlotDto);
   }
 
   @Get()
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved all time slots successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findAll(@Query() queries: ApiLimitResourceQuery) {
     return await this.timeSlotService.findAll(queries);
   }
 
   @Get(':id')
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved time slot successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findOne(@Param('id') id: string) {
     return await this.timeSlotService.findOne(id);
   }
@@ -38,6 +47,9 @@ export class TimeSlotController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Updated time slot successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async update(@Param('id') id: string, @Body() updateTimeSlotDto: UpdateTimeSlotDto) {
     return await this.timeSlotService.update(id, updateTimeSlotDto);
   }
@@ -46,6 +58,9 @@ export class TimeSlotController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Deleted time slot successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async remove(@Param('id') id: string) {
     return await this.timeSlotService.remove(id);
   }
