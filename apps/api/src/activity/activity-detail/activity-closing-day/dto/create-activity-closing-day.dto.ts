ffb4d5402d97d0cd3a74173c0dfef29782471bf2
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 11:37:23 +0200

    doc(api): add activity closing day doc

--- apps/api/src/activity/activity-detail/activity-closing-day/dto/create-activity-closing-day.dto.ts
+++ apps/api/src/activity/activity-detail/activity-closing-day/dto/create-activity-closing-day.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateActivityClosingDayDTO } from '@travel-tailor/types';
 import { IsBoolean, IsDate } from 'class-validator';
 
 export class CreateActivityClosingDayDto implements CreateActivityClosingDayDTO {
   @IsBoolean()
+  @ApiProperty({
+    description: 'Recurrence',
+    type: Boolean,
+    default: false,
+    example: false,
+  })
   recurrence: boolean;
 
   @IsDate()
+  @ApiProperty({
+    description: 'Date',
+    type: Date,
+    default: new Date(),
+    example: new Date(),
+  })
   date: Date;
 }
--- apps/api/src/activity/activity-detail/activity-closing-day/dto/update-activity-closing-day.dto.ts
+++ apps/api/src/activity/activity-detail/activity-closing-day/dto/update-activity-closing-day.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UpdateActivityClosingDayDTO } from '@travel-tailor/types';
 import { IsBoolean, IsDate } from 'class-validator';
 
 export class UpdateActivityClosingDayDto implements UpdateActivityClosingDayDTO {
   @IsBoolean()
+  @ApiProperty({
+      description: 'Recurrence',
+      type: Boolean,
+      default: false,
+    })
   recurrence: boolean;
 
   @IsDate()
+  @ApiProperty({
+      description: 'Closing date',
+      type: Date,
+      default: new Date(),
+    })
   date: Date;
 }
