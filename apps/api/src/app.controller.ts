78bb3a286474752e14dbfc1fda3ecb12f8eaf8ba
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 11:37:33 +0200

    doc(api): add auth doc

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('App')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Success' })
   getHello() {
     return this.appService.getHello();
   }
--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common'
+import { ApiTags, ApiOperation, ApiOkResponse, ApiBadRequestResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler'
 import {
   SigninDTO,
@@ -11,16 +12,24 @@ import { AuthService } from './auth.service'
 
 @Controller('auth')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signin')
   @Throttle(1000, 60)
+  @ApiOperation({ summary: 'Sign in' })
+  @ApiOkResponse({ description: 'Successful login' })
+  @ApiBadRequestResponse({ description: 'Invalid credentials' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   public async signin(@Body() signinUserInputDTO: SigninDTO) {
     return this.authService.signin(signinUserInputDTO)
   }
 
   @Post('signup')
+  @ApiOperation({ summary: 'Sign up' })
+  @ApiOkResponse({ description: 'Successful registration' })
+  @ApiUnauthorizedResponse({ description: 'Invalid input data' })
   @Throttle(1000, 60)
   public signup(@Body() signupUserInputDTO: SignupDTO) {
     return this.authService.signup(signupUserInputDTO)
@@ -28,11 +37,17 @@ export class AuthController {
 
   @Post('forgot-password')
   @Throttle(1000, 60)
+  @ApiOperation({ summary: 'Forgot password' })
+  @ApiOkResponse({ description: 'Password reset email sent' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
   public forgotPassword(@Body() forgotPasswordDto: ForgotPasswordDTO) {
     return this.authService.forgotPassword(forgotPasswordDto)
   }
 
   @Post('reset-password/:token')
+  @ApiOperation({ summary: 'Reset password' })
+  @ApiOkResponse({ description: 'Password successfully reset' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
   @Throttle(1000, 60)
   public resetPassword(@Param('token') token: string, @Body() resetPasswordDto: ResetPasswordDTO) {
     return this.authService.resetPassword(token, resetPasswordDto)
