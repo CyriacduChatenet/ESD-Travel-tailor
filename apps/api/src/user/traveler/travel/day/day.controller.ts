1a03911a8703e5c06749ffd97c9af1d6410fc111
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 16:14:05 +0200

    doc(api): add day doc

--- apps/api/src/user/traveler/travel/day/day.controller.ts
+++ apps/api/src/user/traveler/travel/day/day.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiInternalServerErrorResponse } from '@nestjs/swagger';
 import { ApiLimitResourceQuery } from '@travel-tailor/types';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
@@ -11,6 +12,7 @@ import { Roles } from '../../../../config/decorators/roles.decorator';
 
 @Controller('day')
 @UseGuards(ThrottlerGuard)
+@ApiTags('Day')
 export class DayController {
   constructor(private readonly dayService: DayService) {}
 
@@ -18,18 +20,25 @@ export class DayController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ description: 'Day created successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async create(@Body() createDayDto: CreateDayDto) {
     return await this.dayService.create(createDayDto);
   }
 
   @Get()
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved all days successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findAll(@Query() queries: ApiLimitResourceQuery) {
     return await this.dayService.findAll(queries);
   }
 
   @Get(':id')
   @Throttle(1000, 60)
+  @ApiOkResponse({ description: 'Retrieved day successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async findOne(@Param('id') id: string) {
     return await this.dayService.findOne(id);
   }
@@ -38,6 +47,9 @@ export class DayController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Updated day successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async update(@Param('id') id: string, @Body() updateDayDto: UpdateDayDto) {
     return await this.dayService.update(id, updateDayDto);
   }
@@ -46,6 +58,9 @@ export class DayController {
   @Throttle(1000, 60)
   @UseGuards(JwtAuthGuard)
   @Roles(Role.Traveler, Role.Admin)
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'Deleted day successfully' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   async remove(@Param('id') id: string) {
     return await this.dayService.remove(id);
   }
--- apps/api/src/user/traveler/travel/day/dto/create-day.dto.ts
+++ apps/api/src/user/traveler/travel/day/dto/create-day.dto.ts
@@ -1,12 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsString } from "class-validator";
 
 export class CreateDayDto {
     @IsString()
+    @ApiProperty({
+        description: 'The name of the day',
+        type: String,
+        example: '09:00',
+    })
     startTime?: string;
 
     @IsString()
+    @ApiProperty({
+        description: 'The name of the day',
+        type: String,
+        example: '11:00',
+    })
     endTime?: string;
 
     @IsDate()
+    @ApiProperty({
+        description: 'The name of the day',
+        type: Date,
+        example: '2021-01-01',
+    })
     date: Date;
 }
