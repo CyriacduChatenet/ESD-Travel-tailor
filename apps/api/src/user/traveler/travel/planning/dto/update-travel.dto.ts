63d066496ee4a12e54385b39c106d07ecc11d842
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-06-30 16:17:11 +0200

    doc(api): add planning doc

--- apps/api/src/user/traveler/travel/planning/dto/update-travel.dto.ts
+++ apps/api/src/user/traveler/travel/planning/dto/update-travel.dto.ts
@@ -1,15 +1,36 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsString } from "class-validator";
 
 export class UpdateTravelDto {
     @IsDate()
+    @ApiProperty({
+        description: 'The departure date of the travel',
+        type: Date,
+        example: '2021-01-01',
+    })
     departureDate: Date;
   
     @IsDate()
+    @ApiProperty({
+        description: 'The return date of the travel',
+        type: Date,
+        example: '2021-01-01',
+    })
     returnDate: Date;
 
     @IsString()
+    @ApiProperty({
+        description: 'The departure city of the travel',
+        type: String,
+        example: 'Paris',
+    })
     departureCity: string;
 
     @IsString()
+    @ApiProperty({
+        description: 'The destination city of the travel',
+        type: String,
+        example: 'Paris',
+    })
     destinationCity: string;
 };
