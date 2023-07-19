e8c0377119fa12e6ede005df71fd43b24a9f9428
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-07-19 14:52:41 +0200

    fix(client): restyle create taste component

--- apps/client/components/traveler/tastes/createTasteForm/index.tsx
+++ apps/client/components/traveler/tastes/createTasteForm/index.tsx
@@ -8,89 +8,188 @@ import { useForm } from "react-hook-form";
 import { Player } from "@lottiefiles/react-lottie-player";
 
 interface ICreateTasteForm {
-    name: string;
+  name: string;
 }
 
 export const CreateTasteForm: FC = () => {
-    const [apiErrors, setApiErrors] = useState<{ status?: number }>({});
-    const [tastes, setTastes] = useState<{ name: string }[]>([]);
-    const [submit, setSubmit] = useState<boolean>(false);
+  const [apiErrors, setApiErrors] = useState<{ status?: number }>({});
+  const [tastes, setTastes] = useState<{ name: string }[]>([]);
+  const [submit, setSubmit] = useState<boolean>(false);
 
-    const router = useRouter();
-    const routeParams = usePathname();
-    const { register, handleSubmit, formState: { errors } } = useForm<ICreateTasteForm>();
+  const router = useRouter();
+  const routeParams = usePathname();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ICreateTasteForm>();
 
-    const handleTasteInputChange = async (e: KeyboardEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        const { value } = e.target as HTMLInputElement;
-        await TasteService.createTasteWithRelation(`${process.env.NEXT_PUBLIC_API_URL}`, tastes, routeParams.substring(23, 100), setApiErrors);
-        setTastes([...tastes, { name: value }]);
-    };
-
-    const handleDelete = async (id: string, index: number) => {
-        await TasteService.deleteTaste(`${process.env.NEXT_PUBLIC_API_URL}`, id, setApiErrors);
-        setTastes(tastes.filter((_, i) => i !== index))
-    };
-
-    const onSubmit = async () => {
-        setSubmit(true);
-        if (tastes.length > 0) {
-            await TasteService.createTasteWithRelation(`${process.env.NEXT_PUBLIC_API_URL}`, tastes, routeParams.substring(23, 100), setApiErrors);
-                if(TokenService.getAccessToken()) {
-                    router.push(ROUTES.TRAVELER.DASHBOARD);
-                } else {
-                    router.push(ROUTES.AUTH.SIGNIN);
-                }
-        }
-    };
-
-    return (
-        <div className="max-w-md mx-auto mt-4 col-span-4 md:col-span-8 xl:col-span-12 xl:row-span-6">
-            <label htmlFor="tastes" className="block text-gray-700 font-bold mb-2">
-                Tastes
-            </label>
-            <div className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4">
-                <form action="">
-                    <div>
-                        {tastes.map((taste: Taste, index: number) => (
-                            <div key={index} className="flex justify-around items-center rounded-full bg-blue-500 text-white my-2">
-                                <p>{taste.name}</p>
-                                <button
-                                    className="text-white font-bold py-2 px-4"
-                                    onClick={() => handleDelete(`${String(taste.id)}`, index)}
-                                >
-                                    <Icon icon="material-symbols:close-rounded" />
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                    <div>
-                        <input
-                            {...register("name", {
-                                required: "Taste is required",
-                            })}
-                            id="name"
-                            type="text"
-                            onClick={() => setApiErrors({})}
-                            onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => { setTimeout(() => { handleTasteInputChange(e) }, 2000) }}
-                        />
-                    </div>
-                </form>
-            </div>
-            {submit === true && tastes.length === 0 ? (
-                <p className="text-red-500 text-xs italic">Tags musn&apos;t be empty</p>
-            ) : null}
-            <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                onClick={handleSubmit(onSubmit)}
-            >
-                {submit ? <Player
-                    src='https://assets5.lottiefiles.com/packages/lf20_jk6c1n2n.json'
-                    className="w-12 h-12"
-                    loop
-                    autoplay
-                /> : <>Add Taste</>}
-            </button>
-        </div>
+  const handleTasteInputChange = async (e: KeyboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target as HTMLInputElement;
+    await TasteService.createTasteWithRelation(
+      `${process.env.NEXT_PUBLIC_API_URL}`,
+      tastes,
+      routeParams.substring(23, 100),
+      setApiErrors
     );
+    setTastes([...tastes, { name: value }]);
+  };
+
+  const handleDelete = async (id: string, index: number) => {
+    await TasteService.deleteTaste(
+      `${process.env.NEXT_PUBLIC_API_URL}`,
+      id,
+      setApiErrors
+    );
+    setTastes(tastes.filter((_, i) => i !== index));
+  };
+
+  const onSubmit = async () => {
+    setSubmit(true);
+    if (tastes.length > 0) {
+      await TasteService.createTasteWithRelation(
+        `${process.env.NEXT_PUBLIC_API_URL}`,
+        tastes,
+        routeParams.substring(23, 100),
+        setApiErrors
+      );
+      if (TokenService.getAccessToken()) {
+        router.push(ROUTES.TRAVELER.DASHBOARD);
+      } else {
+        router.push(ROUTES.AUTH.SIGNIN);
+      }
+    }
+  };
+
+  return (
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 col-span-4 md:col-span-8 xl:col-span-12 xl:row-span-6">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      ></div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Create Taste
+        </h2>
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto mt-16 max-w-xl sm:mt-20"
+      >
+        <div className="w-full">
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Name
+            </label>
+            <div className="mt-2.5">
+              <input
+                {...register("name", {
+                  required: "Name is required",
+                })}
+                type="text"
+                name="name"
+                id="name"
+                autoComplete="name"
+                onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => {
+                  setTimeout(() => {
+                    handleTasteInputChange(e);
+                  }, 2000);
+                }}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            {errors.name && (
+              <p className="mt-2 text-red-500 text-xs italic">
+                {errors.name?.message?.toString()}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="mt-8">
+            {tastes.map((taste: Taste, index: number) => (
+              <div
+                key={index}
+                className="flex justify-around items-center rounded-full bg-indigo-600 text-white my-2"
+              >
+                <p>{taste.name}</p>
+                <button
+                  className="text-white font-bold py-2 px-4"
+                  onClick={() => handleDelete(`${String(taste.id)}`, index)}
+                >
+                  <Icon icon="material-symbols:close-rounded" />
+                </button>
+              </div>
+            ))}
+          </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            {submit ? (
+              <Player
+                src="https://assets5.lottiefiles.com/packages/lf20_jk6c1n2n.json"
+                className="w-5 h-5"
+                loop
+                autoplay
+              />
+            ) : (
+              <>Add tastes</>
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
+    // <div className="max-w-md mx-auto mt-4 col-span-4 md:col-span-8 xl:col-span-12 xl:row-span-6">
+    //     <label htmlFor="tastes" className="block text-gray-700 font-bold mb-2">
+    //         Tastes
+    //     </label>
+    //     <div className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4">
+    //         <form action="">
+    //             <div>
+    //                 {tastes.map((taste: Taste, index: number) => (
+    //                     <div key={index} className="flex justify-around items-center rounded-full bg-blue-500 text-white my-2">
+    //                         <p>{taste.name}</p>
+    //                         <button
+    //                             className="text-white font-bold py-2 px-4"
+    //                             onClick={() => handleDelete(`${String(taste.id)}`, index)}
+    //                         >
+    //                             <Icon icon="material-symbols:close-rounded" />
+    //                         </button>
+    //                     </div>
+    //                 ))}
+    //             </div>
+    //             <div>
+    //                 <input
+    //                     {...register("name", {
+    //                         required: "Taste is required",
+    //                     })}
+    //                     id="name"
+    //                     type="text"
+    //                     onClick={() => setApiErrors({})}
+    //                     onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => { setTimeout(() => { handleTasteInputChange(e) }, 2000) }}
+    //                 />
+    //             </div>
+    //         </form>
+    //     </div>
+    //     {submit === true && tastes.length === 0 ? (
+    //         <p className="text-red-500 text-xs italic">Tags musn&apos;t be empty</p>
+    //     ) : null}
+    //     <button
+    //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    //         onClick={handleSubmit(onSubmit)}
+    //     >
+    //         {submit ? <Player
+    //             src='https://assets5.lottiefiles.com/packages/lf20_jk6c1n2n.json'
+    //             className="w-12 h-12"
+    //             loop
+    //             autoplay
+    //         /> : <>Add Taste</>}
+    //     </button>
+    // </div>
+  );
 };
--- apps/client/pages/traveler/taste/create/[id]/index.tsx
+++ apps/client/pages/traveler/taste/create/[id]/index.tsx
@@ -8,7 +8,6 @@ const CreateTastePage: NextPage = () => {
         <Layout title={"Create Your Customized Travel Experience - Tailor-Made Adventures | Travel Tailor"} description={"Craft your perfect travel journey with our user-friendly travel manager. Personalize your trip itinerary based on your unique preferences and interests. Explore exciting activities and create unforgettable memories. Start creating your tailor-made adventure now!"}>
             <main className="px-9 lg:px-32 min-h-screen grid grid-cols-4 md:grid-cols-8 xl:grid-cols-12 xl:row-span-12 pt-20">
                 <div className="xl:row-span-6"></div>
-                <h1 className="font-bold lg:text-2xl col-span-4 lg:col-span-8 xl:col-span-12 xl:row-span-1 text-center">Create Tastes</h1>
                 <CreateTasteForm />
             </main>
         </Layout>
