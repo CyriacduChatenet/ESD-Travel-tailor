2d45aeb03fbfbc78b00fad679c3becec0faf1739
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-09-20 18:21:03 +0200

    fix(client): add padding in signin form input

--- apps/client/components/auth/signinForm/index.tsx
+++ apps/client/components/auth/signinForm/index.tsx
@@ -92,7 +92,7 @@ export const SigninForm: FC = () => {
                 type="email"
                 aria-label="Email address input"
                 autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
               />
               {errors.email && (
                 <p className="mt-2 text-red-500 text-xs italic">
@@ -126,7 +126,7 @@ export const SigninForm: FC = () => {
                 type="password"
                 autoComplete="current-password"
                 aria-label="Password input"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
               />
               {errors.password && (
                 <p className="mt-2 text-red-500 text-xs italic">
